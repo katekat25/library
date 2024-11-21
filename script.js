f56a9613444c6b407771bf72eecbf7e5c1a17907
Kate Schumacher <kate.schumacher@gmail.com>
2024-11-21 12:51:15 -0600

    Adjust book layout to position delete button correctly

--- script.js
+++ script.js
@@ -21,28 +21,36 @@ function addBookToLibrary() {
 }
 
 function putBookOnShelf(newBook) {
-    // console.log("We're gona add smthin!");
     let currentCell = document.querySelector("td.empty");
+    let bookContainer = document.createElement("div");
+    let bookInfoContainer = document.createElement("div");
     let title = document.createElement("div");
     let descriptor = document.createElement("div");
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
     if (currentCell == null) {
-        // console.log("Yikes! Warning!");
         let p = document.createElement("p");
-        // console.log(typeof p);
         p.textContent = "The library is full! Please take out some books!";
         let warningMessage = document.querySelector(".warning-message");
-        // console.log(warningMessage);
         warningMessage.appendChild(p);
         return;
     }
-    //  else if (typeof warningMessage !== undefined) {
-    //     console.log("HEY!");
-    //     return;
-    // }
+    currentCell.appendChild(bookContainer);
+    bookContainer.className = "book-container";
+    currentCell = document.querySelector("td.empty > div");
+    currentCell.appendChild(bookInfoContainer);
+    bookInfoContainer.className = "book-info-container";
+    currentCell = document.querySelector("td.empty > div > div")
     title.textContent = newBook.title;
-    descriptor.textContent = newBook.author + " " + newBook.year;
+    descriptor.textContent = newBook.author + ", " + newBook.year;
     currentCell.appendChild(title);
+    title.className = "book-title";
     title.after(descriptor);
+    currentCell = document.querySelector("td.empty > div");
+    currentCell.appendChild(deleteButton);
+    deleteButton.className = "delete-button";
+    descriptor.className = "book-descriptor";
+    currentCell = document.querySelector("td.empty");
     currentCell.classList.add("full");
     currentCell.classList.remove("empty");
 }
