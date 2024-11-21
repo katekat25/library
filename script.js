3d49c66f97098c7c361c2c6c36a7b564e847e64e
Kate Schumacher <kate.schumacher@gmail.com>
2024-11-21 10:04:06 -0600

    Add a warning message for when library is full

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myLibrary = [];
-const addBookButton = document.querySelector("button");
+const addBookButton = document.querySelector("button.add");
 
 function Book(title, author, year, genre) {
     this.title = title;
@@ -12,7 +12,15 @@ function addBookToLibrary() {
     // console.log("We're gona add smthin!");
     let div = document.createElement("div");
     let currentCell = document.querySelector("td.empty");
-    // console.log(currentCell);
+    console.log(currentCell);
+    if (currentCell == null) {
+        let p = document.createElement("p");
+        p.textContent = "The library is full! Please take out some books!";
+        let warningMessage = document.querySelector(".warning-message");
+        console.log(warningMessage);
+        warningMessage.appendChild(p);
+        return;
+    }
     div.textContent = "heyyy";
     currentCell.classList.add("full");
     currentCell.classList.remove("empty");
