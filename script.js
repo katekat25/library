3a511ebe2095841b80b0950fffa29f33630b1995
Kate Schumacher <kate.schumacher@gmail.com>
2024-11-21 09:36:43 -0600

    Write function to append book info to library cell

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const myLibrary = [];
 const table = document.querySelector("table");
-const div = document.createElement("div");
 
 function Book(title, author, year, genre) {
     this.title = title;
@@ -10,11 +9,22 @@ function Book(title, author, year, genre) {
 }
 
 function addBookToLibrary() {
-    let currentCell = document.querySelector("#empty");
+    // console.log("We're gona add smthin!");
+    let div = document.createElement("div");
+    let currentCell = document.querySelector("td.empty");
+    // console.log(currentCell);
     div.textContent = "heyyy";
-    div.classList.add("full");
-    div.classList.remove("empty");
+    currentCell.classList.add("full");
+    currentCell.classList.remove("empty");
     currentCell.appendChild(div);
 }
+
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
 
 //when a book is added, give it a class
